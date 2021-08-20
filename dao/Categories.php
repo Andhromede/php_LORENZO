24f5389b922ce275461e5ab32c7cd50b9b792565
aldrut <a.drut@free.fr>
2021-08-20 09:30:56 +0200

    add dossier dao

---
+++ dao/Categories.php
@@ -0,0 +1,21 @@
+<?php
+
+class Categories
+{
+    public $categorie;
+    
+
+    public function __construct(string $category)
+    {
+        $this->categorie = $category;
+
+    }
+
+
+
+
+
+
+}
+
+?>
