b87e5962d71df218b6f620142ef707636127e6a0
Andhromede <andhromede@hotmail.fr>
2021-08-20 10:32:34 +0200

    modification structurelle

--- accueil.php
+++ accueil.php
@@ -1,42 +1,46 @@
-    <?php include("header.php")?>
-        <?php include("navbar.php")?>
+<?php   
+    $titre ="Accueil";
+    include_once("header.php");
+    // include("navbar.php");
+    include_once("navbar.php");
+?>
 
-          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-              <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img src="img\carou acceuil\paysage italie.jpg" class="d-block w-100" alt="...">
-                  </div>
-                
-                  <div class="carousel-item">
-                    <img src="img\carou acceuil\Meat-Lovers-Pizza.jpg" class="d-block w-100" alt="...">
-                  </div>
-                
-                  <div class="carousel-item">
-                    <img src="img\carou acceuil\vegetarienne.jpg" class="d-block w-100" alt="...">
-                  </div>
-                
-                  <div class="carousel-item">
-                    <img src="img\carou acceuil\most-expensive-pizza.jpg" class="d-block w-100" alt="...">
-                  </div>
-                
-                  <div class="carousel-item">
-                    <img src="img\carou acceuil\hawaii.jpg" class="d-block w-100" alt="...">
-                  </div>
-                
-               </div>
+<body>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img\carou acceuil\paysage italie.jpg" class="d-block w-100" alt="...">
+            </div>
             
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
+            <div class="carousel-item">
+                <img src="img\carou acceuil\Meat-Lovers-Pizza.jpg" class="d-block w-100" alt="...">
+            </div>
             
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
-             </div> 
+            <div class="carousel-item">
+                <img src="img\carou acceuil\vegetarienne.jpg" class="d-block w-100" alt="...">
+            </div>
+            
+            <div class="carousel-item">
+                <img src="img\carou acceuil\most-expensive-pizza.jpg" class="d-block w-100" alt="...">
+            </div>
+            
+            <div class="carousel-item">
+                <img src="img\carou acceuil\hawaii.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+    </div> 
+</body>
 
-   
-    <?php include("footer.php") ?>
+<?php include("footer.php") ?>
     
     
