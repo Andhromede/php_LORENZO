4b6588dcf2cf56b766f075a61813386a5266fc3c
aldrut <a.drut@free.fr>
2021-08-23 13:32:06 +0200

    update data.php

--- MyData/data.php
+++ MyData/data.php
@@ -39,6 +39,14 @@ $admin = "Administrateur";
 $moderateur = "Moderateur";
 $user = "Utilisateur";
 
+/**
+ * PAGE CONTACT
+ */
+$titreContact = "Contact";
+$titrePourNousContacter = "pour nous contacter";
+$sousTitrePourNousContacter = "Contactez-nous en complétant le formualire :";
+$descriptionPourNousContacter = "N’hésitez pas à nous contacter pour toute question, nous apporter une idée, nous laisser un commentaire, une information utile nous permettant d’améliorer notre qualité de service, vos avis nous intéresses.
+Nous répondront dans les plus brefs délais.";
 
 ?>
 
