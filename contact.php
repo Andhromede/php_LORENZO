2d6fabead6b1a7e2d3fee6ab065e0d651619c399
Andhromede <andhromede@hotmail.fr>
2021-08-27 11:57:03 +0200

    maj carte.php + connexion + page contact en cours

--- contact.php
+++ contact.php
@@ -22,24 +22,132 @@ require_once ("MyData/data.php");
             <?= $descriptionPourNousContacter?>
         </div>
     </div>
+
+   <!--Section: Contact v.2-->
+<section class="mb-4 mt-5">
     <div class="row">
-        <div class="col"></div>
-        <div class="col">
-            <div class="row">
-                <div class="col">
-                    Formualaire</div>
+<form method="post" action="contact.php?action=envoyer">
+      
+        <div class="col-md-6">
+            <div class="md-form mb-0">
+                <input type="radio" class="mr-1 ml-2" name="genre" value="Madame">Madame</input>
+                <input type="radio" class="mr-1 ml-2" name="genre" value="Monsieur">Monsieur</input>
             </div>
-            <div class="row">
-                <div class="col"><input type="text" name="" placeholder="votre nom"> </div>
+        </div>
+
+
+
+        <div class="col-md-6">
+            <div class="md-form mb-0">
+                <input type="text" id="txtEmail" name="email" class="form-control">
+                <label for="email" class="">Votre email *</label>
             </div>
-            <div class="row">
-                <div class="col"><input type="text" name="" placeholder="votre prénom"></div>
-            </div>
-            <div class="row">
-                <div class="col"><input type="text" name="" placeholder="votre "></div>
-            </div>
-            
         </div>
     </div>
 
+                <!--Grid row-->
+    <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="txtNom" name="name" class="form-control">
+                            <label for="name" class="">Votre nom *</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="txtPhone" name="phone" class="form-control">
+                            <label for="phone" class="">Votre téléphone (optionnel)</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="md-form mb-0">
+                            <input type="text" id="txtSujet" name="sujet" class="form-control">
+                            <label for="sujet" class="">Sujet *</label>
+                        </div>
+                    </div>
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-12">
+
+                        <div class="md-form">
+                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                            <label for="message">Votre message *</label>
+                        </div>
+
+                    </div>
+                </div>
+                <!--Grid row-->
+
+           
+
+            <div class="text-center text-md-left">
+                <input type="submit" class="btn btn-primary" />
+            </div>
+         </form>
+            <div class="status"></div>
+        </div>
+        <!--Grid column-->
+
+      
+
+    </div>
+
+</section>
+<!--Section: Contact v.2-->
 </div>
+
+<?php
+    if(isset($_GET["action"]) && $_GET["action"] == "envoyer"){
+
+
+    $civilite = $_POST["genre"];
+    //echo type_of($_POST["genre"]);
+    $nom = $_POST["name"];
+    $email = $_POST["email"];
+    $sujet = $_POST["sujet"];
+    $message = $_POST["message"];
+    $phone = $_POST["phone"];
+
+
+    try {
+
+        $bdh = connexion();
+        $insertContact = $bdh -> query("INSERT INTO `contact` (`civilite`, `nom`, `telephone`, `email`, `sujet`, `message`) VALUES (:civilite,:nom,:telephone, :email,:sujet,:message)");
+
+        $insertContact -> bindParam(':civilite',$civilite,PDO::PARAM_STR);
+        $insertContact -> bindParam(':nom',$nom,PDO::PARAM_STR);
+        $insertContact -> bindParam(':telephone',$phone,PDO::PARAM_STR);
+        $insertContact -> bindParam(':email',$email,PDO::PARAM_STR);
+        $insertContact -> bindParam(':sujet',$sujet,PDO::PARAM_STR);
+        $insertContact -> bindParam(':message',$message,PDO::PARAM_STR);
+
+        $insertContact -> execute();
+
+        $rst = "Donnée enregistrée";
+
+
+    } 
+    catch (Exception $e) {
+        $rst = "Erreur d'enregistrement";
+    }
+
+    }
+
+?>
--- navbar.php
+++ navbar.php
@@ -1,20 +1,7 @@
 
 	<?php
 		require_once("MyData/data.php");
-
-		 // CONNEXION A LA BDD
-		 function connexion(){
-            try {
-                $dbh = new PDO('mysql:host=localhost;dbname=pizza', 'root', '', array(
-                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-                ));
-    
-            } catch (PDOException $e) {
-                print "Erreur !: " . $e->getMessage() . "<br/>";
-                die();
-            } return($dbh);
-            
-        }
+		require_once("db/db.php");
 	?>
 
 
@@ -27,83 +14,66 @@
 		</button>
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-
 			<ul class="navbar-nav ml-5">
 
 				<li class="nav-item mr-4 li">
-
 					<a class="nav-link a-class " href='<?= $linkAccueil ?>'>Accueil</a>
-					<!-- <a class="nav-link a-class " href='accueil.php'>Accueil</a> -->
-
-				</li>
-
-				<li class="nav-item dropdown li mr-4">
-					<a class="nav-link dropdown-toggle a-class" href="carte.php" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Notre carte
-					</a>
-
-					<div class="dropdown-menu bgColor bgMini" aria-labelledby="navbarDropdown">
-						<a class="dropdown-item text-light  a-class" href="">Entrées</a>
-						<!-- <div class="dropdown-divider"></div> -->
-						<a class="dropdown-item text-light a-class" href="">Pizzas</a>
-						<a class="dropdown-item text-light a-class" href="">Dessert</a>
-						<a class="dropdown-item text-light a-class" href="">Boissons</a>
-					</div>
 				</li>
 
 				<li class="nav-item mr-4 li">
-					<a class="nav-link a-class" href="contact.php">Nous contacter</a>
+					<a class="nav-link a-class " href='<?= $linkAccueil ?>'>Notre Carte</a>
 				</li>
 
-					<?php
-date_default_timezone_set('Europe/Paris'); //evite le decalage horraire
+				<?php
+					date_default_timezone_set('Europe/Paris'); //evite le decalage horraire
+					
+					function convertHeure($value){
+						if(!empty($value) ||  $value != null) {
+							$debut = substr($value,0, 5);
+							$fin = substr($value,8, 5);
+							$tab = [$debut, $fin];
+						}return $tab;
+					}
+
+					/* FONCTION D'HEURE D'OUVERTURE & FEMETURE */
+					function isWebsiteOpen() {
+						$heure = microtime(true);
+						$jour = date('N');
+						$dbh = connexion();
+						$horaires = " SELECT * FROM `horaire` ";
+						$rst = false;
+
+						foreach($dbh->query($horaires) as $data){
+							if($data['numJour'] == $jour ){
+								if($data['ouverture'] == 1){
+									if($heure >= strtotime(substr($data['heureAM'],0, 5)) && $heure <= strtotime(substr($data['heureAM'],8, 5))){
+										$rst = true;
+									}elseif ($heure >= strtotime(substr($data['heurePM'],0, 5)) && $heure <= strtotime(substr($data['heurePM'],8, 5))){
+										$rst = true;
+									}else{
+										$rst = false;
+									}
+								}
+							}	
+						}
+
+						// $rst = (date('Gi') >= 1100 && date('Gi') < 2359 || date('N') >= 5 && date('Gi') >= 1700 && date('Gi') < 2359) ? true : false;
+						return $rst;	
+					}
  
-function convertHeure($value){
+					echo((isWebsiteOpen() == true) ?  "<font color='#4CD4B0'>ouvert</font>" : "<font color='#F24D16'>fermé</font>");
+				?>
 
-	if(!empty($value) ||  $value != null) {
-		$debut = substr($value,0, 5);
-		$fin = substr($value,8, 5);
-		$tab = [$debut, $fin];
-	}return $tab;
-	
-
-}
-
-
-        /*HEURE D OUVERTURE ET FEMETURE*/
-	function isWebsiteOpen() {
-		$heure = microtime(true);
-		$jour = date('N');
-		$dbh = connexion();
-		$horaires = " SELECT * FROM `horaire` ";
-		$rst = false;
-
-   	foreach($dbh->query($horaires) as $data){
-		if($data['numJour'] == $jour ){
-			if($data['ouverture'] == 1){
-				if($heure >= strtotime(substr($data['heureAM'],0, 5)) && $heure <= strtotime(substr($data['heureAM'],8, 5))){
-					$rst = true;
-				}elseif ($heure >= strtotime(substr($data['heurePM'],0, 5)) && $heure <= strtotime(substr($data['heurePM'],8, 5))){
-					$rst = true;
-				}else{
-					$rst = false;
-				}
-			}
-		}	
-	}
-	// $rst = (date('Gi') >= 1100 && date('Gi') < 2359 || date('N') >= 5 && date('Gi') >= 1700 && date('Gi') < 2359) ? true : false;
-	 return $rst;	
-}
- 
-	echo((isWebsiteOpen() == true) ?  "<font color='#4CD4B0'>ouvert</font>" : "<font color='#F24D16'>fermé</font>");
-
-?>
 				<li class="nav-item mr-4 li">
 					<a class="nav-link a-class" href="">Nos horaires</a>
 				</li>
 
 				<li class="nav-item mr-4 li">
 					<a class="nav-link a-class" href="">Ou sommes nous?</a>
+				</li>
+
+				<li class="nav-item mr-4 li">
+					<a class="nav-link a-class" href="contact.php">Nous contacter</a>
 				</li>
 
 			</ul>
