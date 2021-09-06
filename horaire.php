96964276b9aa3fdb9c21aaa74d715f16610f76c3
Andhromede <andhromede@hotmail.fr>
2021-09-06 08:34:15 +0200

    maj

--- horaire.php
+++ horaire.php
@@ -11,20 +11,21 @@
 ?>
 	
 <div class="container affichage">
-        <h1 class="text-dark text-center">Nos horaires</h1>
+    <h1 class="text-dark text-center">Nos horaires</h1>
 
-		<div class="border">  
-			<?php foreach($horaires as $horaire): ?>
-			<div class="row mt-5">
-				<div class="col-3 font-weight-bolder h5"><?= $horaire->jour ?> : </div>
-					<?php if($horaire->ouverture == 0) :?>
-						<div class="col bgGrey text-center font-weight-bolder h5">FERMEE </div>
-						
-					<?php else: ?>
-						<div class="col-4 h5 text-center"><?= $horaire->heureAM ?> (matin) </div>
-						<div class="col-4 h5 text-center"><?= $horaire->heurePM ?> (soir) </div>
-					<?php endif;?>
-			</div>
-			<?php endforeach ?>
+	<div class="border">  
+		<?php foreach($horaires as $horaire): ?>
+		<div class="row mt-5">
+			<div class="col-3 font-weight-bolder h5"><?= $horaire->jour ?> : </div>
+				<?php if($horaire->ouverture == 0) :?>
+					<div class="col bgGrey text-center font-weight-bolder h5">FERMEE </div>
+					
+				<?php else: ?>
+					<div class="col-4 h5 text-center"><?= $horaire->heureAM ?> (matin) </div>
+					<div class="col-4 h5 text-center"><?= $horaire->heurePM ?> (soir) </div>
+				<?php endif;?>
 		</div>
+		<?php endforeach ?>
+	</div>
+    
 </div>
